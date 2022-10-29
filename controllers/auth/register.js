15cfd5d45daccdf18f6aac85eb3e41c43c253dc9
MaximIvE <ivanukmaxim@gmail.com>
2022-10-29 22:35:36 +0300

    use bcryptjs for password

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,3 +1,4 @@
+const bcrypt = require("bcryptjs");
 const { User } = require('../../models/user');
 const { requestError } = require('../../helpers');
 
@@ -6,7 +7,8 @@ const register = async (req, res, next) => {
     const user = await User.findOne({ email });
     
     if (user) { throw requestError(409, "Email in use"); };
-    const result = await User.create({ email, password });
+    const hashPassword = await bcrypt.hash(password, 10)
+    const result = await User.create({ email, password: hashPassword });
     res.status(201).json({
         user: {
             email: result.email,
