2ff9b5248180b33c7a863a9f55f700f2620eb629
MaximIvE <ivanukmaxim@gmail.com>
2022-10-31 23:24:08 +0200

    middlware authorization

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const ctrl = require('../../controllers/contacts')
 const {ctrlWrapper} = require('../../helpers')
-const { validateBody, isValidId } = require('../../middlewares')
+const { validateBody, isValidId, authenticate } = require('../../middlewares')
 const {schemas} = require('../../models/contact')
 
 
@@ -10,12 +10,12 @@ router.get('/', ctrlWrapper(ctrl.getAll))
 
 router.get('/:Id', isValidId, ctrlWrapper(ctrl.getById))
 
-router.post('/', validateBody(schemas.schemaAdd), ctrlWrapper(ctrl.add))
+router.post('/', authenticate, validateBody(schemas.schemaAdd), ctrlWrapper(ctrl.add))
 
-router.delete('/:Id', isValidId,  ctrlWrapper(ctrl.removeById))
+router.delete('/:Id', authenticate, isValidId,  ctrlWrapper(ctrl.removeById))
 
-router.put('/:Id', isValidId, validateBody(schemas.schemaUpdate), ctrlWrapper(ctrl.updateById))
+router.put('/:Id', authenticate, isValidId, validateBody(schemas.schemaUpdate), ctrlWrapper(ctrl.updateById))
 
-router.patch('/:Id/favorite', isValidId, validateBody(schemas.schemaUpdateFavorite), ctrlWrapper(ctrl.updateStatusContact))
+router.patch('/:Id/favorite', authenticate, isValidId, validateBody(schemas.schemaUpdateFavorite), ctrlWrapper(ctrl.updateStatusContact))
 
 module.exports = router;
